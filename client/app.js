6fad948b3772bcfd98f7e7631531ed35cb8f165e
Sander Tolsma <github@tolsma.net>
2011-07-18 00:33:31 +0200

    [Client] Small test of album retrieval

--- client/app.js
+++ client/app.js
@@ -8,5 +8,40 @@ Ext.application({
 				html : 'Hello! Welcome to the MediaStore client application.'
 			}]
 		});
+		
+		var albumroot = Ext.ModelMgr.getModel('Albumroot');
+		
+		albumroot.load(0, {
+			success: function(root) {
+				console.log("Root data: ", root);
+			}
+		});
+		
+		albumroot.load('shdfgda73483hjfHHH', {
+			success: function(root) {
+				console.log("Root data: ", root);
+			}
+		});
+
+	}
+});
+
+Ext.define('Albumroot', {
+	extend: 'Ext.data.Model',
+	fields: [
+		{ name: 'id', type: 'int' },
+		{ name: 'children', type: 'array' }
+	],
+	proxy: {
+		type: 'rest',
+		url: 'data/@me/album',
+		noCache: false,
+		appendId: true,
+		headers: {
+			authorization: 'OAUTH2 ' + 'dFcTn_2d8_Tt9u0V0gXC'
+		},
+		reader: {
+			type: 'json',
+		}
 	}
 });
